be67dd8a9a362c71fb5da4c2fd20d56b3a721835
Kathrxn <smithke8@cardiff.ac.uk>
2021-07-30 14:09:05 +0100

    carlist

---
+++ ev/src/carList.js
@@ -0,0 +1,46 @@
+function carList(){
+  return[
+    {
+      name: 'Smart EQ fortwo coupe',
+      range: 60,
+      rapid: 'no',
+      price: 19200,
+      img: 'https://ev-database.uk/img/auto/Smart_fortwo_2020/Smart_fortwo_2020-01-thumb.jpg',
+      link: 'https://ev-database.uk/car/1230/Smart-EQ-fortwo-coupe',
+    },
+    {
+      name: 'Smart EQ forfour',
+      range: 55,
+      rapid: 'no',
+      price: 19795,
+      img: 'https://ev-database.uk/img/auto/Smart_forfour_2020/Smart_forfour_2020-01-thumb.jpg',
+      link: 'https://ev-database.uk/car/1232/Smart-EQ-forfour',
+    },
+    {
+      name: 'Fiat 500e Hatchback 24kWh',
+      range: 100,
+      rapid: 'yes',
+      price: 20495,
+      img: 'https://ev-database.uk/img/auto/Fiat_500e_Hatchback_2020/Fiat_500e_Hatchback_2020-01-thumb.jpg',
+      link: 'https://ev-database.uk/car/1327/Fiat-500e-Hatchback-24-kWh',
+    },
+    {
+      name: 'Smart EQ fortwo cabrio',
+      range: 60,
+      rapid: 'no',
+      price: 21620,
+      img: 'https://ev-database.uk/img/auto/Smart_cabrio_2020/Smart_cabrio_2020-01-thumb.jpg',
+      link: 'https://ev-database.uk/car/1231/Smart-EQ-fortwo-cabrio'
+    },
+    {
+      name: 'MG MG5 EV',
+      range: 180,
+      rapid: 'yes',
+      price: 25095,
+      img: 'https://ev-database.uk/img/auto/MG_MG5_EV/MG_MG5_EV-01-thumb.jpg',
+      link: 'https://ev-database.uk/car/1313/MG-MG5-EV'
+    }
+  ];
+}
+
+export default carList;
