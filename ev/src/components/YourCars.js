38dbc5e88ea7acb637b2e286b5cdae02879647b0
Kathrxn <smithke8@cardiff.ac.uk>
2021-08-05 14:02:11 +0100

    more carsss

--- ev/src/components/YourCars.js
+++ ev/src/components/YourCars.js
@@ -5,12 +5,12 @@ function YourCars({cars, setCars}){
   return (
     <div>
      {cars.map(car => {
-       if(car.rapid === 'yes' && car.range>250 && car.price<50000){
+       if(car.rapid === 'yes' && car.range>250 && car.price>50000){
        return (
          <div key={car.name}>
            <div><a href={car.link}>{car.name}</a></div>
             <img src={car.img} alt='car'/>
-            <div>{car.price}</div>
+            <div>£{car.price.toLocaleString('en-UK')}</div>
           <div>----------------</div>
          </div>
        )
