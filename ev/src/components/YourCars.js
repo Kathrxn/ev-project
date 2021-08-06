c425fd2d83854f82f62dc4d64069e03c479cf2f7
Kathrxn <smithke8@cardiff.ac.uk>
2021-08-06 12:14:40 +0100

    current cars only

--- ev/src/components/YourCars.js
+++ ev/src/components/YourCars.js
@@ -5,7 +5,7 @@ function YourCars({cars, setCars}){
   return (
     <div>
      {cars.map(car => {
-       if(car.rapid === 'yes' && car.range>250 && car.price>50000){
+       if(car.rapid === 'yes' && car.range>250 && car.price>50000 && car.name.includes('coming') === false && car.name.includes('from') === false && car.name.includes('later') === false){
        return (
          <div key={car.name}>
            <div><a href={car.link}>{car.name}</a></div>
