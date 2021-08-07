35ea68fa57f97c70473284c9c79edbd1d276195e
Kathrxn <smithke8@cardiff.ac.uk>
2021-08-07 17:34:37 +0100

    connetcted to api

--- ev/src/App.js
+++ ev/src/App.js
@@ -4,10 +4,11 @@ import Questions from './components/Questions';
 
 function App() {
     const [cars, setCars] = useState(data());
+    const [chargePoints, setChargePoints] = useState([]);
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Questions cars={cars} setCars={setCars}/>
+      <Questions cars={cars} setCars={setCars} chargePoints={chargePoints} setChargePoints={setChargePoints}/>
     </div>
   );
 }
--- ev/src/components/Questions.js
+++ ev/src/components/Questions.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import YourCars from './YourCars';
-
+import axios from 'axios';
 
  //search for chargers in a 5 mile readios https://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/CF15+7PJ/dist/5/
- 
-function Questions({cars, setCars}){
+
+function Questions({cars, setCars, chargePoints, setChargePoints}){
+  useEffect(() => {
+    axios.get('https://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/CF157PJ/dist/5/format/json')
+    .then(chargers => {
+      setChargePoints(chargers.data);
+      console.log(chargers.data);
+    })
+    .catch((err) => console.log(err));
+  },[]);
   return (
     <div>
-      <YourCars cars={cars} setCars={setCars}/>
+      <div>
+      </div>
+      <div>
+        <YourCars cars={cars} setCars={setCars}/>
+      </div>
     </div>
   );
 };
