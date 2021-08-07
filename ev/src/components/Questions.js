0f7d02c84731e1d12f9aa0f40e9c520929e0a520
Kathrxn <smithke8@cardiff.ac.uk>
2021-08-07 16:49:02 +0100

    api search

--- ev/src/components/Questions.js
+++ ev/src/components/Questions.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import YourCars from './YourCars';
 
+
+ //search for chargers in a 5 mile readios https://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/CF15+7PJ/dist/5/
+ 
 function Questions({cars, setCars}){
   return (
     <div>
