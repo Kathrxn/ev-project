8967b5b212ba6e7fc1ac59a8b96fb796f402318c
Kathrxn <smithke8@cardiff.ac.uk>
2021-08-19 20:36:12 +0100

    completed function

--- ev/src/components/Form.js
+++ ev/src/components/Form.js
@@ -4,9 +4,30 @@ import axios from 'axios';
 
 function Form({cars, setCars, chargePoints, setChargePoints}){
   const [postcode, setPostcode] = useState('')
+  const [parking, setParking] = useState('yes')
+  const [work, setWork] = useState('yes')
+  const [range, setRange] = useState('0-99')
+  const [otherCar, setOtherCar] = useState('yes')
+  const [budget, setBudget] = useState('0-20000')
+  const [finalScore, setFinalScore] = useState('')
   //events
   function inputPostcode(e){
     setPostcode(e.target.value);
+  };
+  function inputParking(e){
+    setParking(e.target.value);
+  };
+  function inputWork(e){
+    setWork(e.target.value);
+  };
+  function inputRange(e){
+    setRange(e.target.value);
+  };
+  function inputOtherCar(e){
+    setOtherCar(e.target.value);
+  };
+  function inputBudget(e){
+    setBudget(e.target.value);
   };
   function SearchPostcode(){
       axios.get(`https://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/${postcode}/dist/5/format/json`)
@@ -17,11 +38,50 @@ function Form({cars, setCars, chargePoints, setChargePoints}){
       })
       .catch((err) => console.log(err));
   };
+  function CalculateScore(){
+    let score = 0
+    if (chargePoints > '99'){
+      score = score+15
+    }
+    if (chargePoints > 9 && chargePoints < 100){
+      score = score+10
+    }
+    if (chargePoints > 4 && chargePoints < 10){
+      score = score+5
+    }
+    if (parking === 'yes'){
+      score=score+40
+    }
+    if (work === 'yes'){
+      score=score+15
+    }
+    if (range === '0-99'){
+      score=score+15
+    }
+    if (range === '100-199'){
+      score=score+10
+    }
+    if (range === '200-299'){
+      score=score+5
+    }
+    if (range === '300+'){
+      score=score-10
+    }
+    if(otherCar === 'yes'){
+      score=score+15
+    }
+    if (budget === '0-19999'){
+      score=score-20
+    }
+    setFinalScore(score)
+    console.log(score)
+  }
   return(
     <div>
       <form>
         <label htmlFor="postcode">
           Postcode
+          <h3>What is your postcode?</h3>
           <input
             type='text'
             postcode='postcode'
@@ -29,9 +89,44 @@ function Form({cars, setCars, chargePoints, setChargePoints}){
             onChange={inputPostcode}
           />
         </label>
+        <h3>Do you have off-street parking available at your home?</h3>
+        <select value={parking} onChange={inputParking} id="dropdown">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
+        <h3>Do you have EV charging points available at your work place?</h3>
+        <select value={work} onChange={inputWork} id="dropdown">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
+        <h3>What range would you like your EV to do on a full single charge? (miles)</h3>
+        <select value={range} onChange={inputRange} id="dropdown">
+          <option value="0-99">0-99</option>
+          <option value="100-199">100-199</option>
+          <option value="200-299">200-299</option>
+          <option value="300+">300+</option>
+        </select>
+        <h3>If you were to buy an EV would there also be a petrol/diesel car at your household?</h3>
+        <select value={otherCar} onChange={inputOtherCar} id="dropdown">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
+        <h3>What would be your budget to buy a brand new EV?(GBP)</h3>
+        <select value={budget} onChange={inputBudget} id="dropdown">
+          <option value="0-19999">0-19,999</option>
+          <option value="20000-39999">20,000-39,999</option>
+          <option value="40000-59999">40,000-59,999</option>
+          <option value="60000+">60,000+</option>
+        </select>
       </form>
-      <button onClick={SearchPostcode}>Submit</button>
+      <button onClick={() => { SearchPostcode(); CalculateScore() }}>Submit</button>
       <div>{chargePoints}</div>
+      <div>{parking}</div>
+      <div>{work}</div>
+      <div>{range}</div>
+      <div>{otherCar}</div>
+      <div>{budget}</div>
+      <div>Your score is {finalScore}</div>
       <div>
         <YourCars cars={cars} setCars={setCars}/>
       </div>
