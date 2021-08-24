1eabd1c49be0f2509cac522746f4dd7c352934a9
Kathrxn <smithke8@cardiff.ac.uk>
2021-08-24 15:12:43 +0100

    trying again

--- ev/src/App.js
+++ ev/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='' exact>
+        <Route path='/' exact>
           <Home />
         </Route>
         <Route path='/form'>
