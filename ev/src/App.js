361f10c035c94dd8e6a7ff53e61766c727572e10
Kathrxn <smithke8@cardiff.ac.uk>
2021-08-24 15:34:23 +0100

    sdgsfdg

--- ev/src/App.js
+++ ev/src/App.js
@@ -2,20 +2,22 @@ import React, {useState} from "react";
 import data from './carList';
 import Form from './components/Form';
 import Home from './components/home';
-import {Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
     const [cars, setCars] = useState(data());
   return (
     <div className="App">
-      <Switch>
-        <Route path='/ev-project' exact component ={Home}>
-          <Home />
-        </Route>
-        <Route path='/form'>
-          <Form cars={cars} setCars={setCars}/>
-        </Route>
-      </Switch>
+      <Router>
+        <Switch>
+          <Route path='/ev-project' exact component ={Home}>
+            <Home />
+          </Route>
+          <Route path='/form'>
+            <Form cars={cars} setCars={setCars}/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
